0b1419ee22dc8c6ad3be2453e4de272f9c56a4cd
theory friction practice <jackson.brebnor@gmail.com>
2017-01-18 14:22:54 -0500

    tries stemming

--- src/bligeify.js
+++ src/bligeify.js
@@ -4,3 +4,7 @@ export const yes = () => 'yes';
 export const actor = v => nlp.verb(v).conjugate().actor;
 export const gerund = v => nlp.verb(v).conjugate().gerund;
 export const bligeify = verb => actor(verb).concat('ation');
+const txt = 'there was a sickness in the hospital';
+
+console.log('txt', nlp.statement(txt));
+console.log('nlp.text', nlp.statement('sickness'));
